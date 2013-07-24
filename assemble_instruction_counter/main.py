ff6f230dda6d3b0267aef9525c4f1fe887ca05d8
cntimothy <cntimothy1990@gmail.com>
2013-07-24 22:27:05 +0800

    ENH:add support for show unsupported instructions

--- assemble_instruction_counter/main.py
+++ assemble_instruction_counter/main.py
@@ -3,16 +3,18 @@ import operator
 
 for i in range(1, 21):
     filename = str(i) + '.c'
-    os.system('/usr/local/mipsel-robin-elf/bin/gcc -S ' + filename)
+    os.system('/usr/local/mipsel-robin-elf/bin/gcc -S -o1 ' + filename)
 
 dic = {}
+
+ins_list = ['add', 'addu', 'sub', 'subu', 'and', 'or', 'xor', 'nor', 'slt', 'sltu', 'sll', 'srl', 'sra', 'sllv', 'srlv', 'srav', 'jr', 'addi', 'addiu', 'andi', 'ori', 'xori', 'lw', 'sw', 'beq', 'bne', 'slti', 'sltiu', 'lui', 'j', 'jal'] 
 
 detail = open('detail.txt', 'w')
 
 for i in range(1, 21):
 	count = 0
 	filename = str(i) + '.s'
-	detail.write('==========================='+filename+'===========================\n')
+	detail.write('=========================================='+filename+'==========================================\n')
 	f = open(filename)
 	LineNo = 1
 	for line in f:
@@ -39,4 +41,16 @@ f = open('result.txt', 'w')
 sorted_list = sorted(dic.iteritems(), key=operator.itemgetter(1), reverse=True)  
 for item in sorted_list:
 	f.write(str(item) + '\n')
+	
+f.write('\n\n********************************************\n\n')	
+f.write('Not support instruction:\n\t')
+count = 1
+for key in dic.keys():
+	if not key in ins_list:
+		f.write(key + ', ')
+		if count == 10:
+			f.write('\n')
+			count = 1
+		count += 1
 f.close()
+
